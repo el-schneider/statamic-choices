be1b1575f75ad4cfd1c957fb2adb263634f22534
el-schneider <mail@jonaslist.de>
2026-05-09 21:56:42 +0200

    fix: tighten frontend type checks

--- resources/js/v6/addon.ts
+++ resources/js/v6/addon.ts
@@ -2,8 +2,6 @@
 import '../shared/choices.css'
 import ChoicesFieldtype from './components/ChoicesFieldtype.vue'
 
-declare const Statamic: any
-
 Statamic.booting?.(() => {
   Statamic.$components?.register('choices-fieldtype', ChoicesFieldtype)
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,13 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vitest/config'
 
+const statamicUiMock = new URL('./tests/js/mocks/statamic-ui.ts', import.meta.url).pathname
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@statamic/cms/ui': '/tests/js/mocks/statamic-ui.ts',
+      '@statamic/cms/ui': statamicUiMock,
     },
   },
   test: {
